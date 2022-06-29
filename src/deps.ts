2801ab938bd67cfaa31ad486ced6aaa910faf29c
Paulo Vidal <paulo.vidal@bitfy.app>
2022-06-28 21:18:31 -0300

    add polkadot/api deps

---
+++ src/deps.ts
@@ -0,0 +1,9 @@
+// std
+export * as log from 'https://deno.land/std@0.143.0/log/mod.ts';
+
+// Third party modules
+
+export {
+   ApiPromise,
+   WsProvider,
+} from 'https://deno.land/x/polkadot@0.0.1/api/mod.ts';
